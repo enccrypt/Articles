8c9971d22e4146e8b4adecc24e4aed8efcb6bdf7
John Doe <johndoe@example.com>
2024-04-19 17:42:23 +0300

    19 april

---
+++ resources/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+    abracadabra
+</template>
+<script>
+    export default {
+    data() { return { msg: null } },
+        created() {
+            window.Echo.channel('my-channel').listen('ArticleCreateEvent', (name) => {
+                console.log(name);
+                this.msg=name.name;
+                // alert('Добавлена новая статья!');
+            })
+        }
+    }
+</script>
+
